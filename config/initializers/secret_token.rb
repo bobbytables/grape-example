188b0b3a4c62e32eec952383a3bab8e1fb1fb000
Robert Ross <robert@postandbeam.is>
2012-05-10 11:25:09 -0700

    Initial Commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GrapeExample::Application.config.secret_token = '4cf4c4dc021014986339465c231a7a626671a54cf6a80ef83203feb0453b1be5c7b020630bb3aafdfe9610e97ffc003a735e2d91e9d447232c1e6bd5d39d1746'
